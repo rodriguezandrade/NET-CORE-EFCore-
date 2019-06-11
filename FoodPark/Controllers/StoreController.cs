38feb69de6aecb8224bc526036e1d8282c400677
rodriguezandrade <1140117@unav.edu.mx>
2019-06-10 23:22:06 -0500

    Excersice with Net Core and EFCore

---
+++ FoodPark/Controllers/StoreController.cs
@@ -0,0 +1,32 @@
+﻿using FoodPark.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FoodPark.Controllers
+{
+    [Route("api/stores/")]
+    public class StoreController : Controller
+    {
+        private readonly ModelContext _context;
+
+        public StoreController(ModelContext context)
+        {
+            _context = context;
+        }
+
+
+        [Route("get")]
+        public IEnumerable<Store> Get()
+        {
+            IEnumerable<Store> stores = new List<Store>() {
+                 new Store()
+                 {
+                     FullName= "test"
+                 }
+            };
+
+            return stores;
+        }
+
+    }
+}
---
+++ FoodPark/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodPark.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
