a7909a87526546cccff951246aedb0d6857c9b7e
Jonathan Andrade <1140117@unav.edu.mx>
2019-06-16 13:48:24 -0500

    Minor changes

--- Core/Models/Category.cs
+++ Core/Models/Category.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Core.Models
+﻿namespace Core.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public int Name { get; set; }
-        public int IdPhotoResource { get; set; }
+        public int ImageResourceId { get; set; }
+
+        public ImageResource ImageResource { get; set; }
     }
 }
